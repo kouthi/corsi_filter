dde83dff5ada71e8a76d1653e5aad7faa42ca327
kouthi <kouthi@users.noreply.github.com>
2016-10-14 20:10:57 +0900

    add chomp() for each <STDIN> reading

--- corsi_filter.pl
+++ corsi_filter.pl
@@ -1,16 +1,20 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-use 5.010
+use 5.010;
 
 my ($fname_input, $fname_output);
-$fname_input = $ARGV[0]
-$fname_output = $ARGV[1]
+$fname_input = $ARGV[0];
+$fname_output = $ARGV[1];
 
-print "Npixel: "; my $num_pixel = <STDIN>;
-print "Cd [fF]: "; my $cd = <STDIN>;
-print "Cq [fF]: "; my $cq = <STDIN>;
-print "Rq [kOhm]: "; my $rq = <STDIN>;
-print "Cg [pF]: "; my $cg = <STDIN>;
+my %params;
 
+print "Npixel: "; chomp($params{"Npixel"} = <STDIN>); 
+print "Cd [fF]: "; chomp($params{ "Cd"} = <STDIN>);
+print "Cq [fF]: "; chomp($params{ "Cq"} = <STDIN>);
+print "Rq [kOhm]: "; chomp($params{ "Rq"} = <STDIN>);
+print "Cg [pF]: "; chomp($params{ "Cg"} = <STDIN>);
 
+foreach (keys %params) {
+    say "$_: $params{$_}"
+}
