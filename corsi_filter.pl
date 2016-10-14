a418b18a9ebf5772fc58d4cc78b000d0ca96780c
kouthi <kouthi@users.noreply.github.com>
2016-10-14 20:41:29 +0900

    read each input lines successfully

--- corsi_filter.pl
+++ corsi_filter.pl
@@ -7,14 +7,25 @@ my ($fname_input, $fname_output);
 $fname_input = $ARGV[0];
 $fname_output = $ARGV[1];
 
-my %params;
-
-print "Npixel: "; chomp($params{"Npixel"} = <STDIN>); 
-print "Cd [fF]: "; chomp($params{ "Cd"} = <STDIN>);
-print "Cq [fF]: "; chomp($params{ "Cq"} = <STDIN>);
-print "Rq [kOhm]: "; chomp($params{ "Rq"} = <STDIN>);
-print "Cg [pF]: "; chomp($params{ "Cg"} = <STDIN>);
-
+my %params = read_parameters();
 foreach (keys %params) {
     say "$_: $params{$_}"
+}
+
+open my $input, "<", $fname_input or die ("error: could not open $!");
+say $input;
+while (<$input>) {
+    chomp;
+    my @line_args = split, ' ';
+}
+
+
+sub read_parameters {
+    my %params;
+    print "Npixel: "; chomp($params{"Npixel"} = <STDIN>); 
+    print "Cd [fF]: "; chomp($params{ "Cd"} = <STDIN>);
+    print "Cq [fF]: "; chomp($params{ "Cq"} = <STDIN>);
+    print "Rq [kOhm]: "; chomp($params{ "Rq"} = <STDIN>);
+    print "Cg [pF]: "; chomp($params{ "Cg"} = <STDIN>);
+    return %params;
 }
