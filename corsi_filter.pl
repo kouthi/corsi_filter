a67e07b3e393fdb0102aa535581c030b8217fc72
kouthi <kouthi@users.noreply.github.com>
2016-10-15 15:51:17 +0900

    hoge

--- corsi_filter.pl
+++ corsi_filter.pl
@@ -7,25 +7,39 @@ my ($fname_input, $fname_output);
 $fname_input = $ARGV[0];
 $fname_output = $ARGV[1];
 
-my %params = read_parameters();
+my $cd = { name => "Cd", unit => "fF" };
+say $cd;
+my $cq = { name => "Cq", unit => "fF" };
+say $cq;
+my $rq = { name => "Rq", unit => "kOhm" };
+say $rq;
+my $cg = { name => "Cg", unit => "pF" };
+say $cg;
+my %params = read_parameters($cd, $cq, $rq, $cg);
+say "-----";
 foreach (keys %params) {
-    say "$_: $params{$_}"
+    say "$_: $params{$_}";
 }
+say "-----";
 
 open my $input, "<", $fname_input or die ("error: could not open $!");
-say $input;
+my $flag_InstName = 0;
 while (<$input>) {
     chomp;
-    my @line_args = split, ' ';
+    my @line_args = split, /' '/;
+    say $line_args[2] if $flag_InstName;
+    $flag_InstName = $line_args[1] eq "InstName" ? 1 : 0;
 }
 
-
 sub read_parameters {
+    say @_;
     my %params;
     print "Npixel: "; chomp($params{"Npixel"} = <STDIN>); 
-    print "Cd [fF]: "; chomp($params{ "Cd"} = <STDIN>);
-    print "Cq [fF]: "; chomp($params{ "Cq"} = <STDIN>);
-    print "Rq [kOhm]: "; chomp($params{ "Rq"} = <STDIN>);
-    print "Cg [pF]: "; chomp($params{ "Cg"} = <STDIN>);
+    for (my $i = 0; $i < @_; $i++) {
+        my $ref = shift;
+        say $ref;
+        #print "$ref->{'name'} [$ref->{'unit'}]: "; 
+        #chomp($params{$ref->{'name'}} = <STDIN>);
+    }
     return %params;
 }
